c1b030f9cb918b015047fabb278eaebb1c2f62bb
mahsa <mahsaadlooo@gmail.com>
2023-09-05 00:58:58 +0330

    complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js,ts}"],
+  content: ["./*.{html,js,ts}","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
